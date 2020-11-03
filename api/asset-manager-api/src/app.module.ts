6bc1fecd4016545a08b6d0d562f12a5d09bdcb1b
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2020-11-03 02:56:03 +0100

    Initial Commit         Create new asset         Transfer asset         get new address         swagger ui         Get a new user address         Save user information to local sqllite database.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- api/asset-manager-api/src/app.module.ts
+++ api/asset-manager-api/src/app.module.ts
@@ -1,10 +1,34 @@
+import { User } from './models/user.model';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
+import { AuthGuard } from './guards/auth.guard';
+import { AssetsController } from './controllers/assets.controller';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AssetsService } from './services/assets.service';
+import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'AssetManager.db',
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [
+    AssetsController,
+    UserController,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    },
+    AssetsService,
+    UserService,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
